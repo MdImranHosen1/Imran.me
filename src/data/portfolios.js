3963cb9a3958383e4d4f6fe945b1a78a368ec477
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2024-08-15 23:16:35 +0600

    "Add Project"

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -12,7 +12,7 @@ export const portfolios = {
     {
       type: "featured",
       name: "Hotel Management Full Stack",
-      image: require("./image/hotel_mangement.png"),
+      image: require("./image/hotel_mangement2.png"),
       description:
         "The same project of I build with Node JS and React JS. Everything is dynamic here.",
       demo: "https://github.com/imrancse190/Frontend-ReactJS-NodeJS-Assignment",
@@ -46,10 +46,11 @@ export const portfolios = {
     {
       type: "regular",
       name: "EcoSync",
-      description: "This application ",
+      description:
+        "EcoSync, developed during Code Samurai 2024, streamlines garbage collection from homes to designated grounds, ensuring efficient and eco-friendly waste management.",
       demo: "https://github.com/MdImranHosen1/CS24-p2-BSMRSTU_Prefect_Number",
       source: "https://github.com/MdImranHosen1/CS24-p2-BSMRSTU_Prefect_Number",
-      tools: ["ExpressJS", "NextJS", "MongooDB", "Tailwind CSS"],
+      tools: ["ExpressJS", "ReactJS", "MongooDB", "Tailwind CSS"],
     },
     {
       type: "regular",
@@ -59,17 +60,68 @@ export const portfolios = {
       source: "https://github.com/MdImranHosen1/BarcodeGenerator",
       tools: ["ReactJS", "Tailwind CSS"],
     },
+    {
+      type: "regular",
+      name: "Todo App",
+      description:
+        "A simple and efficient Todo application built using modern web technologies like React, Redux, and Material-UI. ",
+      demo: "https://todo-app-redux-iota.vercel.app/",
+      source: "https://github.com/MdImranHosen1/todo-app-redux/tree/main",
+      tools: ["Vite", "Tailwind CSS"],
+    },
+    {
+      type: "featured",
+      name: "Helicopter war",
+      image: require("./image/helicopterWar.png"),
+      description:
+        "About this is 2D action battlefield game. The I have to save my helicopter by destroying enemy helicopter and tank. This project build using c++ language and use IGraphics. Visual studio 2013 use for building system.",
+      demo: "https://github.com/MdImranHosen1/Helicopter-War-Project",
+      source: "https://github.com/MdImranHosen1/Helicopter-War-Project",
+      tools: ["C++", "IGraphics", "Visual studio 2013"],
+    },
   ],
   Others: [
     {
+      type: "regular",
+      name: "TODO-App",
+      // image: require("./image/duaAppProject.png"),
+      description: "This todo app build using Django and React JS",
+      demo: "https://github.com/MdImranHosen1/Django-React-TODO-App",
+      source: "https://github.com/MdImranHosen1/Django-React-TODO-App",
+      tools: ["Django", "ReactJS", "Tailwind CSS"],
+    },
+    {
+      type: "regular",
+      name: "Note-App",
+      // image: require("./image/duaAppProject.png"),
+      description: "This Note app build using Django and React JS",
+      demo: "https://github.com/MdImranHosen1/Django-React-Notes-App",
+      source: "https://github.com/MdImranHosen1/Django-React-Notes-App",
+      tools: ["Django", "ReactJS", "Tailwind CSS"],
+    },
+    {
       type: "featured",
-      name: "DuaApp",
-      image: require("./image/duaAppProject.png"),
+      name: "Household Smart Share Web",
+      image: require("./image/householdSmartShare.png"),
       description:
-        "Cross-platform desktop tool for the developers, does some useful job and make life easy",
-      demo: "https://dua-next-js-node-js.vercel.app/",
-      source: "https://github.com/MdImranHosen1/Dua-next.js-node.js",
-      tools: ["NextJS", "Tailwind CSS"],
+        "We made this application where people can upload unused items in this application. Those who need these items can request through this application.",
+      demo:
+        "https://github.com/MdImranHosen1/Project-3.2-House-hold-smart-share",
+      source:
+        "https://github.com/MdImranHosen1/Project-3.2-House-hold-smart-share",
+      tools: ["NodeJS", "ReactJS"],
+    },
+    {
+      type: "featured",
+      name: "Household Smart Share Android",
+      image: require("./image/HouseholdSmartShareA.png"),
+      description:
+        "We made this application where people can upload unused items in this application. Those who need these items can request through this application.",
+      demo:
+        "https://github.com/MdImranHosen1/Project-2.2-House-hold-smart-share-Android-",
+      source:
+        "https://github.com/MdImranHosen1/Project-2.2-House-hold-smart-share-Android-",
+      tools: ["Java", "Android Studio"],
     },
   ],
 }
